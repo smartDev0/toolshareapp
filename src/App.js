8bf72e30b3e9178e53f89f62eedd12e626b272ad
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2020-09-25 03:25:39 +0100

    import react native vector icons

--- src/App.js
+++ src/App.js
@@ -7,7 +7,27 @@ import ReduxThunk from 'redux-thunk';
 import HomeScreen from './components/HomeScreen';
 import RouterComponent from './Router';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import Foundation from 'react-native-vector-icons/Foundation';
+import Entypo from 'react-native-vector-icons/Entypo';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
 Icon.loadFont();
+Feather.loadFont();
+Ionicons.loadFont();
+MaterialCommunityIcons.loadFont();
+MaterialIcons.loadFont();
+AntDesign.loadFont();
+Fontisto.loadFont();
+SimpleLineIcons.loadFont();
+Foundation.loadFont();
+Entypo.loadFont();
+EvilIcons.loadFont();
 console.disableYellowBox = true;
 export default class App extends Component {
   render() {
